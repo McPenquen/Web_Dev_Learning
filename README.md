a8698ceb66f3f3e5ad51f5b763bbc1d78b291b6f
McPenquen <majda.calk@outlook.com>
2021-01-17 00:22:59 +0100

    Finished chapter 6

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # TO RUN THE APP DO:
 '
 .\env\Scripts\activate
-
+set FLASK_APP=myapp.py
+set FLASK_ENV=development
+python -m flask run
 
 
 
@@ -14,8 +16,13 @@ deactivate
 'python -m flask run --host=0.0.0.0' - tun the app
 
 
-# Controlling the virtual environment 
+# Controlling the virtual environment
 'py -m venv env' - get the venvenv
 '.\env\Scripts\activate' - activate the venvenv
 'where python' - check the location of the virtual env
 'deactivate' - close the virtual env
+
+# Requests
+'curl -i -X POST http://127.0.0.1:5000/.../..' - post to the route .../..
+
+
--- ad-web-dev-labs/myapp.py
+++ ad-web-dev-labs/myapp.py
@@ -1,5 +1,8 @@
-from flask import Flask, redirect, url_for, abort
+from flask import Flask, redirect, url_for, abort, request
+import time
 app = Flask(__name__)
+
+# debugg tool: request.method, request.path, request.form
 
 @app.route('/')
 def root():
@@ -12,10 +15,74 @@ def welcome():
     end = '">'
     return '<h3>Welcome warrior...</h3><br>'+start+url+end, 200
 
+@app.route('/profile/<name>')
+def profile(name):
+    return "<h3>Name: %s <br>Age: 22<br> Class: Warrior</h3>" % name
+
+@app.route('/profile/picture', methods=['POST', 'GET'])
+def picture():
+    if request.method == 'POST':
+        f = request.files['datafile']
+        f.save('static/profile.jpg')
+        return '<img src="'+ url_for('static', filename='profile.jpg') +'">' 
+    else:
+        pic = '<img src="'+ url_for('static', filename='profile.jpg') +'">' 
+        page = ''' 
+        <html><body>
+            <form action="" method="post" name="form" enctype="multipart/form-data">
+                <input type="file" name="datafile"/>
+                <input type="submit" name="submit" id="submit"/>
+            </form>
+        </body><html>
+        '''
+        return pic + page, 200
+
 @app.route('/village')
 def village():
     return 'Here is the village Somewood'
 
+@app.route('/village/soldier', methods=['GET','POST'])
+def soldier():
+    if request.method == 'POST':
+        print(request.form)
+        name = request.form['name']
+        return "Get out of here %s and don't do any trouble" % name
+    else:
+        page= ''' 
+        <html><body>
+            <form action="" method="post" name="form>
+                <label for="name">What's your name?</label>
+                <input type="text" name="name" id="name"/>
+                <input type="submit" name="submit" id="submit"/>
+            </form>
+        </body><html>
+        '''
+        return page
+
+@app.route('/village/smith', methods=['GET','POST'])
+def smith():
+    if request.method == 'POST':
+        return "You have interacted with the smith."
+    else:
+        return "You see the smith."
+
+@app.route('/village/wizard')
+def wizard():
+    return "Oogy loogy boo yoo.. bye... pufff"
+
+@app.route('/village/find/')
+def find_in_village():
+    thing = request.args.get('name', '')
+    if thing == '':
+        return "Are you looking for something in the village?"
+    elif thing == 'wizard':
+        return redirect('/village/wizard')
+    elif thing == 'smith':
+        return redirect('/village/smith')
+    elif thing == 'soldier':
+        return redirect('/village/soldier')
+    else:
+        return "%s is nowhere to be found" % thing
 
 @app.route('/town')
 def town():
